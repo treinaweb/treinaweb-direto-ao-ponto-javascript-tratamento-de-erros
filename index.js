f90ddb82f70fa35b5d0d545b453b87aa32d7ef53
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-16 13:35:53 -0300

    01:03 - Propriedades do objeto Erro

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
-var loading = false;
 
 try {
-    console.log('tentando executar...');
-    loading = true;
-    // throw 'erro desconhecido';
+    console.log('Primeiro console');
+    // const meuArray = new Array(1000000000000000);
+    throw new Error("erro desconhecido", {
+        cause: "causa do erro"
+    });
 
-    console.log('final da execução');
+    console.log('Segundo console');
 } catch (erro) {
-    console.log('executou com erro');
-} finally {
-    loading = false;
-    console.log('finally');
+    // console.log(erro);
+    console.log("Nome: ", erro.name);
+    console.log("Message: ", erro.message);
+    console.log("Cause: ", erro.cause);
+    console.log("Stack : ", erro.stack);
 }
 
-console.log('final do código', loading);
+console.log('Final da execução do programa');
