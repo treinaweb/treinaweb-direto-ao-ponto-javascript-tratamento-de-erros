e2557cd935b06c30ffa559562a228925b492ff31
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-16 13:37:19 -0300

    01:07 - Entendendo a Call Stack

--- index.js
+++ index.js
@@ -1,56 +1,25 @@
-class MeuErro extends Error {
-    constructor(message, props) {
-        super(message, props);
-        this.name = "MeuErro";
-    }
+function main() {
+    F1();
+    console.log("função main");
 }
 
-async function asyncFunction() {
-    return await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (true) {
-                resolve("Sucesso");
+function F1() {
+    F2();
+    try {
+        new Array(1000000000000);
+    } catch (error) {
 
-            } else {
-
-                reject(new MeuErro("Erro desconhecido"));
-            }
-
-
-        }, 1000);
-    });
+    }
+    console.log("função F1");
 }
 
-asyncFunction().then((result) => {
-    throw "novo erro desconhecido";
-    new Array(100000000000);
-    console.log(result);
-}).catch((erro) => {
-    if (erro instanceof MeuErro) {
-        console.log(erro.name);
-    }
+function F2() {
+    F3();
+    console.log("função F2");
+}
 
-    if (erro instanceof RangeError) {
-        console.log(erro.name);
-    }
-    if (!(erro instanceof Error)) {
+function F3() {
+    console.log("função F3");
+}
 
-        console.log(erro);
-    }
-
-});
-
-console.log('final da execução do código');
-
-// async function runAsync() {
-//     try {
-//         console.log("carregando...");
-//         const result = await asyncFunction();
-//         console.log('executou: ', result);
-//     } catch (erro) {
-//         console.log(erro.name);
-//     }
-
-// }
-
-// runAsync();
+main();
