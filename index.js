74ed357babab5dd29ac4cc28337b6a45cf247d51
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-16 13:35:19 -0300

    01:01 - Lançando um erro

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Primeiro console');
+
+
+console.log('Segundo console');
+
+// const meuArray = new Array(1000000000000000);
+
+throw "erro desconhecido";
+
+console.log('Final da execução do programa');
