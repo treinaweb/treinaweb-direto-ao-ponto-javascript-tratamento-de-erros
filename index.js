46c8e68bb818a8f1817c9f58fbaea1e1c2b71d23
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-02-16 13:36:18 -0300

    01:04 - Tratando erros específicos

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
+class MeuErro extends Error {
+    constructor(message, props) {
+        super(message, props);
+        this.name = "MeuErro";
+    }
+}
 
+function handleErro(erro) {
+    if (erro instanceof MeuErro) {
+        console.log("tratando MeuErro");
+    }
 
-try {
-    console.log('Primeiro console');
-    // const meuArray = new Array(1000000000000000);
-    throw new Error("erro desconhecido", {
-        cause: "causa do erro"
-    });
-
-    console.log('Segundo console');
-} catch (erro) {
-    // console.log(erro);
+    if (erro instanceof RangeError) {
+        console.log("tratando o RangeError");
+    }
     console.log("Nome: ", erro.name);
     console.log("Message: ", erro.message);
     console.log("Cause: ", erro.cause);
-    console.log("Stack : ", erro.stack);
+}
+
+function fetchErro() {
+    if (Math.random() > 0.5) {
+        throw new MeuErro("erro desconhecido", {
+            cause: "causa do erro"
+        });
+    } else {
+        new Array(1000000000000000);
+    }
+}
+
+try {
+    fetchErro();
+} catch (erro) {
+    handleErro(erro);
 }
 
 console.log('Final da execução do programa');
